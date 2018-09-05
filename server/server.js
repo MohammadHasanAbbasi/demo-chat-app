b4f91c1e3408d80fd7e267b24d2b8cc96f85adb2
mhd.hasan <mohammad1376@gmail.com>
2018-09-05 23:33:26 +0430

    socket added

--- server/server.js
+++ server/server.js
@@ -1,12 +1,41 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http =require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-var app = express();
 
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection',(socket)=>{
+    console.log('User connected');
+    socket.emit('newMessage',"Welcome to chat room");
+
+    socket.broadcast.emit('newMessage',{
+      text:"New user joined"
+    })
+
+    socket.on('createMessage',function(message){
+          console.log(message);
+          io.emit('newMessage',{
+             text: message.text,
+              name:message.name,
+              createdAt: new Date().getUTCDate()
+          });
+    });
+
+    socket.on('disconnect',()=>{
+      console.log("User disconnected.")
+
+    });
+
+  });
+
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
